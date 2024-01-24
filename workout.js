69dc19f148b4f63441040adfc45f7e6bc9445fd8
Matt Sinnock <mattsinnock@gmail.com>
2024-01-25 12:26:31 +1300

    Fix audio

--- workout.js
+++ workout.js
@@ -117,7 +117,22 @@ function beep(duration, frequency) {
 }
 
 document.getElementById('startButton').addEventListener('click', function() {
+    // Start the workout
     updateExercise();
     intervalId = setInterval(countdown, 1000);
+
+    // Unlock audio on mobile devices
+    if (!isAudioUnlocked) {
+        // Create and play an empty buffer to unlock the audio
+        var buffer = audioCtx.createBuffer(1, 1, 22050);
+        var source = audioCtx.createBufferSource();
+        source.buffer = buffer;
+        source.connect(audioCtx.destination);
+        source.start(0);
+
+        // Update the flag
+        isAudioUnlocked = true;
+    }
+
     this.style.display = 'none'; // Hide the start button
 });
